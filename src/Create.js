fd5571fabf9887627de2d48f08fd66e27a913b7c
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-01 22:22:41 +0400

    feat: add onSubmit form function

--- src/Create.js
+++ src/Create.js
@@ -6,10 +6,16 @@ const Create = () => {
     // const [author, setAuthor] = useState('mario')
     const [author, setAuthor] = useState('mario')
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author }
+        console.log(blog)
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
