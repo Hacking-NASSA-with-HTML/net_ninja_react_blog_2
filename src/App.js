9cb6212ba399b3a84c18b5c97887767fc0050417
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-01 19:56:09 +0400

    feat: add Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
-            <div className="content">
-                <Home />
-            </div>
+            <BrowserRouter>
+                <Navbar />
+                <div className="content">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
