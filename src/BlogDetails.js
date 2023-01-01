2e0d0aeb1f04e1c5ecf5a418343843cdb954be1f
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-01 21:35:53 +0400

    feat: add styling to Blog's Details module

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom"
+import useFetch from "./useFetch"
 
 const BlogDetails = () => {
     const { id } = useParams()
+    const { data: blog, isPending, err } = useFetch('http://localhost:8000/blogs/' + id)
+
     return (
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            {err && <div>{err}</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     )
 }
